23af766fc1f1fe24e55e6d300acca8bad3cace15
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-08-02 10:46:33 +0300

    Project init

---
+++ src/test/java/com/turkcell/staj/StajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.staj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

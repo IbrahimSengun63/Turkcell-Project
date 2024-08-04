3f5ffd0fdc0077d9258aa176de1ad912b6508241
Alperen Aydin <alprn13aydn@gmail.com>
2024-08-04 23:58:08 +0300

    Add `Package` entity

---
+++ src/main/java/com/turkcell/staj/entities/Package.java
@@ -0,0 +1,30 @@
+package com.turkcell.staj.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "packages")
+public class Package {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "package_name")
+    private String packageName;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "price")
+    private double price;
+    @Column(name = "status")
+    private boolean status;
+    @OneToMany(mappedBy = "package")
+    private List<Transaction> transactions;
+}

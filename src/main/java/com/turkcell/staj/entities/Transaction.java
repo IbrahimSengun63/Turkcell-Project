288d77e72c17c13bcc70288e833e81aaf2db7fca
Alperen Aydin <alprn13aydn@gmail.com>
2024-08-05 00:23:00 +0300

    Add `Transaction` entity

---
+++ src/main/java/com/turkcell/staj/entities/Transaction.java
@@ -0,0 +1,39 @@
+package com.turkcell.staj.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "packages")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "package_id")
+    private int packageId;
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "price")
+    private double price;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+    @Column(name = "created_date", updatable = false)
+    @CreationTimestamp
+    private LocalDate createdDate;
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne()
+    @JoinColumn(name = "package_id")
+    private Package package_; // package keyword cannot be used as a variable name
+}

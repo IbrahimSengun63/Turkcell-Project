f6145b5f32a02f1b293bcc5e41f13f76db8360b8
Alperen Aydin <alprn13aydn@gmail.com>
2024-08-04 23:32:59 +0300

    Add `User` entity

--- src/main/java/com/turkcell/staj/entities/User.java
+++ src/main/java/com/turkcell/staj/entities/User.java
@@ -1,4 +1,28 @@
 package com.turkcell.staj.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "balance")
+    private double balance;
+    @OneToMany(mappedBy = "user")
+    private List<Transaction> transactions;
 }

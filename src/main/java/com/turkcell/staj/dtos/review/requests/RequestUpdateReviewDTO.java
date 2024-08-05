b792035e5c64da975c3805b66c11d7bd757fb15b
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-08-05 15:35:04 +0300

    Review `Update` DTOs added.

---
+++ src/main/java/com/turkcell/staj/dtos/review/requests/RequestUpdateReviewDTO.java
@@ -0,0 +1,33 @@
+package com.turkcell.staj.dtos.review.requests;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestUpdateReviewDTO {
+    @NotNull(message = "Review can't be null")
+    @Min(value = 1, message = "Id must be a positive integer greater than or equal to 1")
+    private int reviewId;
+    @NotNull(message = "Offer can't be null")
+    @Min(value = 1, message = "Id must be a positive integer greater than or equal to 1")
+    private int offerId;
+    @NotNull(message = "User can't be null")
+    @Min(value = 1, message = "Id must be a positive integer greater than or equal to 1")
+    private int userId;
+    @Min(value = 1, message = "Rating must be in the range {1, 5}")
+    @Max(value = 5, message = "Rating must be in the range {1, 5}")
+    private int rating;
+    private String comment;
+    // TODO: current date checking rule
+    private LocalDate createdDate;
+}
---
+++ src/main/java/com/turkcell/staj/dtos/review/responses/ResponseUpdateReviewDTO.java
@@ -0,0 +1,21 @@
+package com.turkcell.staj.dtos.review.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseUpdateReviewDTO {
+    private int reviewId;
+    private int offerId;
+    private int userId;
+    private int rating;
+    private String comment;
+    private LocalDate createdDate;
+}

f75bf43b4b6505f03a7d8e01e7cb31b6323cb1eb
BBerkan <berkann.b2@gmail.com>
2024-08-05 12:52:13 +0300

    user updated

--- src/main/java/com/turkcell/staj/dtos/user/requests/RequestUpdateUserDTO.java
+++ src/main/java/com/turkcell/staj/dtos/user/requests/RequestUpdateUserDTO.java
@@ -1,7 +1,8 @@
 package com.turkcell.staj.dtos.user.requests;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,9 +17,11 @@ public class RequestUpdateUserDTO {
 
     private int userId;
     @NotBlank
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
     @NotBlank
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String surname;
-    @Positive
+    @PositiveOrZero(message = "Balance must be zero or positive")
     private double balance;
 }

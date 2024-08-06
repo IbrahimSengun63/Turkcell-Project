5d26967fba3950aefaebe09fc4030d83095098d1
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-08-06 14:53:50 +0300

    `Transaction Business rules` constructor bug fixed.

--- src/main/java/com/turkcell/staj/business/rules/TransactionBusinessRules.java
+++ src/main/java/com/turkcell/staj/business/rules/TransactionBusinessRules.java
@@ -5,11 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public final class TransactionBusinessRules {
-    // Private constructor to prevent instantiation
-    private TransactionBusinessRules() {
-        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
-    }
-
     public static void checkIfUserHasEnoughBalance(double userBalance, double price) {
         if (userBalance < price) {
             throw new BusinessException("User does not have enough balance for this transaction.");

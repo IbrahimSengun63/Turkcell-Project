f24db0b88725c9d8bf36336423b536d8346409e0
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-08-06 11:51:03 +0300

    `User Repository` and `Offer Repository` created.

--- src/main/java/com/turkcell/staj/repositories/UserRepository.java
+++ src/main/java/com/turkcell/staj/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.turkcell.staj.repositories;
 
-public interface UserRepository {
+import com.turkcell.staj.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
 }

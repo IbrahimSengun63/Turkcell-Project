5eb4dc1c17670a747a80f11781350691102897c7
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-08-08 18:34:03 +0300

    `Transaction Controller` end points modified.

--- src/main/java/com/turkcell/staj/controllers/TransactionController.java
+++ src/main/java/com/turkcell/staj/controllers/TransactionController.java
@@ -19,26 +19,26 @@ import org.springframework.web.bind.annotation.*;
 public class TransactionController {
     private final TransactionService transactionService;
 
-    @PostMapping("/v1/add")
+    @PostMapping("/add")
     public ResponseEntity<ResponseAddTransactionDTO> addTransaction(@RequestBody @Valid RequestAddTransactionDTO requestAddTransactionDTO) {
         ResponseAddTransactionDTO responseAddTransactionDTO = this.transactionService.addTransaction(requestAddTransactionDTO);
         return ResponseEntity.ok(responseAddTransactionDTO);
     }
 
-    @PostMapping("/v1/update/{id}")
+    @PostMapping("/update/{id}")
     public ResponseEntity<ResponseUpdateTransactionDTO> updateTransaction(@PathVariable @Valid @Min(value = 1) int id, @RequestBody @Valid RequestUpdateTransactionDTO requestUpdateTransactionDTO) {
         requestUpdateTransactionDTO.setTransactionId(id);
         ResponseUpdateTransactionDTO responseUpdateTransactionDTO = this.transactionService.updateTransaction(requestUpdateTransactionDTO);
         return ResponseEntity.ok(responseUpdateTransactionDTO);
     }
 
-    @GetMapping("/v1/history/{id}")
+    @GetMapping("/history/{id}")
     public ResponseEntity<GetUserTransactionsWrapper> getHistory(@PathVariable @Valid @Min(value = 1) int id){
         GetUserTransactionsWrapper wrapper = this.transactionService.getHistory(id);
         return ResponseEntity.ok(wrapper);
     }
 
-    @PostMapping("/v1/transaction/{transactionId}/return")
+    @PostMapping("/{transactionId}/return")
     public ResponseEntity<ResponseReturnTransactionDTO> returnTransaction(
             @PathVariable @Valid @Min(value = 1) int transactionId,
             @RequestParam @Valid @Min(value = 1) int userId){

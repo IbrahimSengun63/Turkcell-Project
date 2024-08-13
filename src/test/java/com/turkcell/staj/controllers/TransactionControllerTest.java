3ee42fc537f18ec145b291f1cd20dd43c73e71bf
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-08-13 11:50:49 +0300

    added 3 test cases for `transactions/user/{userId}` endpoint

--- src/test/java/com/turkcell/staj/controllers/TransactionControllerTest.java
+++ src/test/java/com/turkcell/staj/controllers/TransactionControllerTest.java
@@ -2,10 +2,12 @@ package com.turkcell.staj.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.turkcell.staj.business.abstracts.TransactionService;
+import com.turkcell.staj.controllers.responseWrappers.GetUserTransactionsWrapper;
 import com.turkcell.staj.core.enums.Status;
 import com.turkcell.staj.dtos.transaction.requests.RequestAddTransactionDTO;
 import com.turkcell.staj.dtos.transaction.requests.RequestUpdateTransactionDTO;
 import com.turkcell.staj.dtos.transaction.responses.ResponseAddTransactionDTO;
+import com.turkcell.staj.dtos.transaction.responses.ResponseGetAllUserTransactionDTO;
 import com.turkcell.staj.dtos.transaction.responses.ResponseReturnTransactionDTO;
 import com.turkcell.staj.dtos.transaction.responses.ResponseUpdateTransactionDTO;
 import org.junit.jupiter.api.AfterEach;
@@ -19,11 +21,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -232,6 +235,75 @@ class TransactionControllerTest {
 
         // Verify
         verify(transactionService, never()).returnTransaction(anyInt(), anyInt());
+    }
+
+    @Test
+    void shouldGetUserTransactionHistory() throws Exception {
+        // Arrange
+        int userId = 1;
+        List<ResponseGetAllUserTransactionDTO> transactions = List.of(
+                new ResponseGetAllUserTransactionDTO(1, userId, 1, 22.0, Status.COMPLETED, LocalDate.now())
+        );
+        GetUserTransactionsWrapper wrapper = new GetUserTransactionsWrapper();
+        wrapper.setUserTransactions(transactions);
+        wrapper.setTotalPurchaseHistory(22.0);
+
+        // when
+        when(transactionService.getHistory(userId)).thenReturn(wrapper);
+
+        // Act
+        mockMvc.perform(get("/api/transactions/user/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userTransactions[0].transactionId").value(1))
+                .andExpect(jsonPath("$.userTransactions[0].userId").value(userId))
+                .andExpect(jsonPath("$.userTransactions[0].offerId").value(1))
+                .andExpect(jsonPath("$.userTransactions[0].price").value(22.0))
+                .andExpect(jsonPath("$.userTransactions[0].status").value("COMPLETED"))
+                .andExpect(jsonPath("$.userTransactions[0].createdDate").isNotEmpty())
+                .andExpect(jsonPath("$.totalPurchaseHistory").value(22.0))
+                .andReturn();
+
+        // Verify
+        verify(transactionService, times(1)).getHistory(userId);
+    }
+
+    @Test
+    void shouldReturnEmptyTransactionHistoryWhenUserHasNotTransactions() throws Exception {
+        // Arrange
+        int userId = 1;
+        GetUserTransactionsWrapper wrapper = new GetUserTransactionsWrapper();
+        wrapper.setUserTransactions(Collections.emptyList());
+        wrapper.setTotalPurchaseHistory(0.0);
+
+        // when
+        when(transactionService.getHistory(userId)).thenReturn(wrapper);
+
+        // Act
+        mockMvc.perform(get("/api/transactions/user/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userTransactions").isEmpty())
+                .andExpect(jsonPath("$.totalPurchaseHistory").value(0.0))
+                .andReturn();
+
+        // Verify
+        verify(transactionService, times(1)).getHistory(userId);
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenGetUserTransactionsPathVariableValidationFails() throws Exception {
+        // Arrange
+        int invalidUserId = -1;
+
+        // Act
+        mockMvc.perform(get("/api/transactions/user/{userId}", invalidUserId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        // Verify
+        verify(transactionService, never()).getHistory(anyInt());
     }
 
 
